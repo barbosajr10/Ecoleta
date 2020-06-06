2e25a712a1b12f456c87f33270bebb3eedd0b660
Gabriel Barbosa <gabriellbarbosamimi@gmail.com>
2020-06-06 15:36:59 -0300

    Ecoleta

---
+++ public/scripts/intro.js
@@ -0,0 +1,64 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// let myvar = 'Hello'
+// const myconst = 'Hello'
+// document.write(myvar + myconst)
+
+//string
+//"Isso é uma String ou com ''"
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean True ou False
+// const btrue = true
+// const bFalse = false
+// document.write(btrue)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa ={
+//   altura:'1.77m',
+//   idade:'17',
+//   solteiro:true,
+//   correr(){
+//     document.write()
+//     document.write('Executar uma função lógica de correr')
+//   }
+// } 
+
+// pessoa.correr()
+
+//  Array(vetores)
+// const colecaoDeBolinhas = ["blue","green",1 ,{name: 'My Name'}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funções - funcionalidades - Atalhos - Reuso de código
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// sayMyName("Gabriel, ")
+// sayMyName("Mirella, ")
+// sayMyName("Nene")
+
+// const notaFinal = 5
+
+// if( notaFinal < 5) {
+//    document.write("Reprovado")
+// } else {
+//    document.write("Aprovado")
+// }
+
+// loop repetição
+// for (i=1; i<=10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
+
+
+
+
